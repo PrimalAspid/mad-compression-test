efa54d5a99b9fe16109c710ca40cdc0cb94b8470
PrimalAspid <vasilykrylov07@gmail.com>
2025-07-31 11:36:46 +0200

    some updates

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import os #test
 from openai import OpenAI
 import pandas
 
+api_key_file = open("api_key.txt")
+
 #admin stuff
 llm = OpenAI(
     # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
+    api_key= api_key_file.readline().strip()
 )
 
 #getting the test dataset
@@ -30,7 +32,7 @@ def vanilla_MAD(question):
   initial_answer = llm_prompt(question)
   condensed_answer = llm_prompt("condense '"+ initial_answer + "' into 1 sentence or less.")
 
-  debator_prompt = "The question is '" + question + "' and an answer is '" + condensed_answer + "'. You are a debator, debating whether this answer is correct or not."
+  debator_prompt = "You are a debater. Hello and welcome to the debate competition. It’s not necessary to fully agree with each other’s perspectives, as our objective is to find the correct answer. The debate topic is stated as follows: The question is '" + question + "' and an answer is '" + condensed_answer + "' Is this answer correct?"
   affirmative_prompt = " You are on the affermative side. Please express your viewpoints. "
   negative_prompt = " You are on the negative side. You disagree with the affirmative side's points. Provide your own answer, and argue why it is better."
   judge_prompt = "You are a moderator. There will be two debators involved in a debate competition. They will present their answers and discuss their perspectives on an answers to question '" + question + "', the affermative side is arguing for the answer '"+initial_answer+"'. Your job is to evaluate both side's answers and either say if the debate is inconclusive, or state clearly the answer that is clearly better than the other. If you choose the latter, leave your answer in the form '[Quick thoughts] therefore the answer is [answer]' The debate goes as follow: "
@@ -39,12 +41,10 @@ def vanilla_MAD(question):
   #actual debate.
   running = True
   round_number = 0
-  #initally affermative states position
-  conversation_history += "affirmative side: \n" + llm_prompt(debator_prompt + affirmative_prompt + "\n" + conversation_history) + "\n"
 
   while running:
-    conversation_history += "negative side: \n" + llm_prompt(debator_prompt + negative_prompt + "\n" + conversation_history) + "\n"
     conversation_history += "affirmative side: \n" + llm_prompt(debator_prompt + affirmative_prompt + "\n" + conversation_history) + "\n"
+    conversation_history += "negative side: \n" + llm_prompt(debator_prompt + negative_prompt + "\n" + conversation_history) + "\n"
 
     #judge:
     verdict = llm_prompt(judge_prompt + "\n" + conversation_history + "\n Verdict: ")
@@ -58,8 +58,8 @@ def vanilla_MAD(question):
       running = False
       verdict = llm_prompt(out_of_time_judge_prompt + "\n" + conversation_history)
 
-
-  return llm_prompt("extract the actual answer from this explanation of an answer. Give it in the form of a number: " + verdict)
+  final_answer = llm_prompt("extract the actual answer from this explanation of an answer. Give it in the form of a number: " + verdict)
+  return final_answer
 
 #testing
 question_count = 0 # total num questions answered - in case something unexpected happens, I still want % correct. Also used as index
@@ -81,14 +81,14 @@ while running:
       correct_questions += 1
 
     #breaks the code at 100, because communication with openai api is reallly slow, and it would take about 8 hours to finish the whole dataset
-    if question_count >= 1000:
+    if question_count >= 20:
       running = False
     
     #----
     question_count += 1
      
     #just printing something to give me a progress bar
-    print(f"{question_count} : {is_correct} : {correct_questions / question_count * 100} ")
+    print(f"{question_count} : {is_correct} : {correct_questions / question_count * 100} : {current_answer} : {llm_answer}")
 
     
   except: #breaks when there is an error(either eof, openai api running out of tokens, etc)
